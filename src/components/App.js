55c60a43a9da0b87759af1abe8f3c33c50181a46
Adil Khan <119532381+AdilKhan53@users.noreply.github.com>
2023-07-27 13:20:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,23 @@ import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
-  const [count, setCount] = useState(1);
+  const [counter, setCounter] = useState(1);
   const [tempClass, setTempClass] = useState('');
 
-  function increaseFn() {
-    setCount(count => count + 1);
+  function incrementFn() {
+    setCount(counter => counter + 1);
   }
 
-  function decreaseFn() {
-    setCount(count => count - 1);
+  function decrementFn() {
+    setCount(counter => counter - 1);
   }
 
   useEffect(() => {
-    if (count % 5 == 0 && count % 3 == 0) {
+    if (counter % 5 == 0 && counter % 3 == 0) {
       setTempClass('fizzbuzz')
-    } else if (count % 3 == 0) {
+    } else if (counter % 3 == 0) {
       setTempClass('fizz')
-    } else if (count % 5 == 0){
+    } else if (counter % 5 == 0){
       setTempClass('buzz') 
     } else {
       setTempClass('normal')
@@ -28,9 +28,9 @@ const App = () => {
   
   return (
     <div id="main">
-    <button onClick={increaseFn}>Increase</button>
-    <div className={tempClass}>{count}</div>
-    <button onClick={decreaseFn}>Decrease</button>
+    <button onClick={incrementFn}>Increment</button>
+    <div className={tempClass}>{counter}</div>
+    <button onClick={decrementFn}>Decrement</button>
 
     </div>
   )
